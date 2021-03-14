3e8fc5835ef6d3d4bbca78dd853b69df0f931df6
Skaivolker <rmt.codemail@gmail.com>
2021-03-14 10:06:24 +0200

    smt

--- src/main/java/com/example/demo/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -1,4 +1,19 @@
 package com.example.demo.configuration;
 
-public class SecurityConfiguration {
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+
+    }
 }
